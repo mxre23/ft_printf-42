9531dc6fa43914ce0fa29b9b13038befd62c7bbd
Joanna Cristina More Ojeda <jmore-oj@car3s5.42barcelona.com>
2024-03-13 21:36:13 +0100

    new stuff

--- printf.h
+++ printf.h
@@ -1,26 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmore-oj <jmore-oj@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/11 21:38:30 by jmore-oj          #+#    #+#             */
-/*   Updated: 2024/03/13 21:34:32 by jmore-oj         ###   ########.fr       */
+/*   Created: 2024/03/13 21:06:10 by jmore-oj          #+#    #+#             */
+/*   Updated: 2024/03/13 21:08:11 by jmore-oj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static void	ft_putstr(char *s, int fd)
-{
-	result = ft_putstr_fd(char *s, int fd);
-	return (result);
-}
+#ifndef PRINTF_H
+# define PRINTF_H
+# include <unistd.h>
+# include <stdarg.h>
+# include "libft.h"
 
-
-
-
-
-int	ft_printf(char const *, ...)
-{
-
-}
+#endif
