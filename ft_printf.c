a157db42b5c5ee10ff0cb87b30b19a74121cab61
Joanna Cristina More Ojeda <jmore-oj@car7s6.42barcelona.com>
2024-03-21 22:28:44 +0100

    finished first part and header

--- ft_printf.c
+++ ft_printf.c
@@ -6,31 +6,68 @@
 /*   By: jmore-oj <jmore-oj@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 21:38:30 by jmore-oj          #+#    #+#             */
-/*   Updated: 2024/03/16 17:59:41 by jmore-oj         ###   ########.fr       */
+/*   Updated: 2024/03/21 22:24:45 by jmore-oj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static char	*ft_check_type(char *s)
+#include "ft_printf.h"
+
+static int	ft_vselect(char const s, va_list args, int count)
 {
-	size_t	len;
-	size_t	i;
+	if (s == 'c')
+		count = ft_putcharr(va_arg(args, int), count);
+	else if (s == 's')
+		count = ft_putstrr(va_arg(args, char *), count);
+	else if (s == 'p')
+		count = ft_putptr(va_arg(args, void *), count);
+	else if (s == 'i' || s == 'd')
+		count == ft_putnbrr(va_arg(args, int), count);
+	else if (s == 'u')
+		count = ft_putuns(va_arg(args, unsigned int), count);
+	else if (s == 'x')
+		count = ft_puthexalow(va_arg(args, unsigned int), count);
+	else if (s == 'X')
+		count = ft_hexaup(va_arg(args, long int), count);
+	else if (s == '%')
+		count = ft_putchar('%', count);
+	else
+		count = -1;
+	return (count);
+}
+
+static int	ft_check(char const *s, va_list args, int count)
+{
+	int	i;
 
 	i = 0;
-	len = ft_strlen(s);
-	while(*s != % && i < len)
+	while (s[i])
 	{
-		write(1, &s[i++], 1);
+		if (s[i] == '%')
+		{
+			i++;
+			count = ft_vselect((char)s[i], args, count);
+			if (count == -1)
+				return (-1);
+		}
+		else
+		{
+			if (write(1, &s[i], 1) == -1)
+				return (-1);
+			count++;
+		}
+		i++;
 	}
+	return (count);
 }
- 
-int	ft_printf(char const *, ...)
+
+int	ft_printf(char const *s, ...)
 {
 	va_list	args;
-	int	c;
+	int		c;
 
 	c = 0;
 	va_start(args, s);
-	c = ft_checktype(s, args, c);
+	c = ft_check(s, args, c);
 	va_end(args);
-	return(c);
+	return (c);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,27 @@
 /*   By: jmore-oj <jmore-oj@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 21:06:10 by jmore-oj          #+#    #+#             */
-/*   Updated: 2024/03/13 21:08:11 by jmore-oj         ###   ########.fr       */
+/*   Updated: 2024/03/21 22:24:52 by jmore-oj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <stddef.h>
 # include <unistd.h>
 # include <stdarg.h>
 # include "libft.h"
+
+int		ft_printf(char const *s, ...);
+int		ft_putcharr(char const c, int count);
+int		ft_putnbrr(int n, int count);
+int		ft_puthexalow(unsigned long hex, int count);
+int		ft_puthexaup(unsigned int hexa, int count);
+int		ft_putptr(void *ptr, int count);
+int		ft_putstrr(char const *s, int count);
+int		ft_putuns(unsigned int n, int count);
 
 #endif
